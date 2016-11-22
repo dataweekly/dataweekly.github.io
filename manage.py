d440a4541befeb78096d3dc4b69c1be89b5faad3
zhourunlai <u201316277@hust.edu.cn>
2016-11-22 10:47:01 +0800

    init

--- manage.py
+++ manage.py
@@ -24,7 +24,7 @@ def new_post(title):
     """
 
     yaml_header = 'title: %s' % title + '\n' +\
-                  'date: %s' % str(datetime.now())[:-3] + '\n\n'
+                  'date: %s' % str(datetime.now())[:10] + '\n\n'
 
     filename = title.replace(' ','_') + '.md'
     filepath = os.path.join(pagesdir, filename)
